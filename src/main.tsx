43756d522cc1c44effb24c974352c55d1872a61a
56_harsh tambade <harsh.if56@tpoly.in>
2024-11-17 14:15:37 +0530

    bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
-import Analytics from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
